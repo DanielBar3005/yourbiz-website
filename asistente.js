0e0cea5de93a360cf18a53386a3bf771589416ad
DanielBar3005 <168190316+DanielBar3005@users.noreply.github.com>
2026-01-29 11:51:11 -0500

    Add files via upload

---
+++ asistente.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  const script = document.createElement("script");
+  script.src =
+    "https://files.bpcontent.cloud/2025/12/16/14/20251216143340-Z6EA17GH.js";
+  script.defer = true;
+
+  document.body.appendChild(script);
+});
